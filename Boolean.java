feb85105c5440366141db0ed03431b789c624ad9
mahathi8790 <akmahathi1319@gmail.com>
2023-08-17 23:55:07 +0530

    Create Boolean.java

---
+++ Boolean.java
@@ -0,0 +1,10 @@
+class Boolean
+{
+	public static void main(String[] args)
+{		//boolean b=true;
+		//boolean b='true';
+		//boolean b=8;
+		  boolean b=FALSE;
+		System.out.println("The value of b = " +b);
+}
+}
