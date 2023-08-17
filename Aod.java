135e52960fdb79a5eb0657764657f2e45c5d59e6
mahathi8790 <akmahathi1319@gmail.com>
2023-08-17 23:51:32 +0530

    Create Aod.java

---
+++ Aod.java
@@ -0,0 +1,17 @@
+class Aod
+{
+   public static void main(String[] args)
+{
+   int a=20,b=10;
+   int sum=a+b;
+   int difference=a-b;
+   int product=a*b;
+   int quotient=a/b;
+   int remainder=a%b;
+   System.out.println("Adiition of given numbers " +sum);
+   System.out.println("Substraction of given numbers " +difference);
+   System.out.println("Multiplpication of given numbers " +product);
+   System.out.println("Division of given numbers " +quotient);
+   System.out.println("Remainder of given numbers " +remainder);
+}
+}
